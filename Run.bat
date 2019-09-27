054db58a5f9c9871df76e34d81dcea9762d14ef7
FreeFly <ryanjvanslyke@gmail.com>
2019-09-27 10:53:36 -0400

    Delete Run.bat

--- Run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Batch file for running python pomodoro timer in the current directory
-rem Place in shell:startup with the program to start it on startup for windows
-set file=pythonPomodoroGUI.py
-echo Opening %file%
-python %file%
-exit()
