f03c658e397a724fb3e4c3afaca182a0c4eb2c7a
FreeFlyFall <ryanjvanslyke@gmail.com>
2021-04-06 18:46:11 -0400

    Complete rewrite

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # pythonPomodoro
 
 Simple pomodoro timer that requires little user input.
-
-2 versions:
-- Command line interface
-- Tkinter graphical user interface
--- pythonPomodoroCLI.py
+++ pythonPomodoroCLI.py
@@ -1,88 +1,150 @@
-import winsound
-import time
-import os
+import winsound, time, os, re
 
 '''
-Pomodoro timer created to require little user interaction and remove the need to have a browser open
-'''
+Pomodoro timer created to require little user interaction and remove the need to have a browser open.
+Enter times for work, break, and long break in minutes, and the number of iterations before the long break time activates,
+separated by spaces.
 
+e.g. "1h45 15 30 3" to work for 1 hour 45 minutes with a 15 min break for 3 cycles, followed by a cycle with a 30 minute break.
+
+Times can be input as #h#, #h#m, #h, #m, or #. Number with no label are taken as minutes.
+    For 1 hour and 45 minutes, acceptable inputs are 1h45, and 1h45m.
+    For 1 hour, acceptable input is 1h.
+    For 30 minutes, acceptable inputs are 30m, and 30.
+'''
 # -- FUNCTIONS -------
 # Define function passed the type of time that sets that time
-def set_time(timeType):
+def set_times():
     while True:
+        retry = False
         try:
-            # Get integer input from user for the specific time type
-            inputData = input('Enter ' + timeType + ' hours, minutes, and seconds separated by a space: ')
-            h, m, s = inputData.split(' ')
-            timeData = [h, m, s]
-            for i, data in enumerate(timeData):
-                timeData[i] = int(timeData[i])
-                if(timeData[i] > 60):
-                    print("Input " + str(timeData[i]) + " > 60; flooring.")
-                    timeData[i] = 60
-            h, m, s = timeData
-            return h, m, s
-        # Print error and return to getting integer input
+            try:
+                w, b, l, i = input('> ').strip().split(' ')
+            except KeyboardInterrupt:
+                exit()
+            except:    
+                print('Enter 4 times')
+                continue
+            for string in [w, b, l]:
+                hours = re.search('(\d+)h',string)
+                if hours:
+                    hours = hours.group().replace('h','')
+                minutes = re.search('h/d+$|\d+m|\d+$', string) # Match 1+ digits after h followed by end of line, 1+ digits followed by m, or 1+ digits followed by end of line
+                if minutes:
+                    minutes = minutes.group().replace('m','')
+                if not hours and not minutes:
+                    print(f'Invalid input: {string}')
+                    retry = True
+                    continue
+                for string in minutes, hours:
+                    if string:
+                        if int(string) < 1:
+                            print(string + ' is less than 1')
+                            retry = True
+                if hours and int(hours) > 24:
+                    print(f'{hours}h is too long')                        
+                if minutes and int(minutes)/60 > 24: 
+                    print(f'{minutes}m is too long')
+            if retry:
+                continue
+            iterations = re.search('\d+$',i)
+            if not iterations or int(iterations.group()) < 1:
+                print(f'{i} is not a valid iteration count')
+                continue
+                
+            print(f'Work: {w}, Break: {b}, Long Break: {l}, Iterations: {i}')
+
+            try:
+                confirm = input('Continue? (Y/n): ').upper()
+                if confirm != 'Y' and confirm != '':
+                    continue
+            except KeyboardInterrupt:
+                exit()
+
+            return cycle_time(w), cycle_time(b), cycle_time(l), i
+        # Print error and return to getting input
         except Exception as e: #ValueError:
             print(e)
-            continue
 
-# Define function passed state and times that returns data
-def change_state():
-    # If the user is working
-    if isWorking:
-        # Set & return the variables for the break state
-        return 'Take a break!', 'BreakSound', breakHours, breakMinutes, breakSeconds
-    # Else, the user is not working
+class Timer():
+    def __init__(self, seconds):
+        self.start_time = time.time()
+        self.total_seconds = int(seconds)
+    def elapsed(self):
+        return int(time.time()-self.start_time)
+    def is_next_second(self, current_second):
+        return self.elapsed() > current_second
+    def value(self):
+        display_seconds = int((self.total_seconds - self.elapsed()) % 60)
+        display_minutes = int((self.total_seconds/60 - self.elapsed()/60) % 60)
+        display_hours = int(self.total_seconds/60/60 - self.elapsed()/60/60)
+        if display_seconds == 0 and display_minutes == 0:
+            display_hours += 1
+        return zero_pad_int(display_hours) + ':' + zero_pad_int(display_minutes) + ':' + zero_pad_int(display_seconds)
+    def done(self):
+        return self.elapsed() >= self.total_seconds
+
+def start_timer(mins, iteration_type):
+    seconds = int(mins) * 60
+    timer = Timer(seconds)
+    current_second = 0
+    print(' '.join([timer.value(),iteration_type]))
+    while not timer.done():
+        if timer.is_next_second(current_second):
+            current_second += 1
+            print(' '.join([timer.value(),iteration_type]))
+        time.sleep(0.1) # Polling rate to update countdown
+    if iteration_type == 'work':
+        play_sound('BreakSound')
     else:
-        # Set & return the variables for the work state
-        return 'Get back to work!', 'WorkSound', workHours, workMinutes, workSeconds
+        play_sound('WorkSound')
 
-# -- PROGRAM START -------
-
-print('Press Ctrl+C to exit during the countdown.')
-# Get and set both the work and break times using the set_time() function
-workHours, workMinutes, workSeconds = set_time("work")
-breakHours, breakMinutes, breakSeconds = set_time("break")
-# Create counter variables to track time and initialize them to be the value for working state
-counterHours, counterMinutes, counterSeconds = workHours, workMinutes, workSeconds
-isWorking = True
-
-# Loop to repeatedly run countdown logic and inform user
-while True:
-    # If the countdown is running
-    if (counterHours >= 0 and counterMinutes >= 0 and counterSeconds >= 0):
-        # Wait one second, decrement the counter, and print the remaining time
-        print(counterHours, counterMinutes, counterSeconds)
-        if(counterSeconds > 0):
-            counterSeconds -= 1
-        elif(counterMinutes > 0):
-            counterMinutes -= 1
-            counterSeconds = 59
-        elif(counterHours > 0):
-            counterHours -= 1
-            counterMinutes = 59
-            counterSeconds = 59
-        time.sleep(1)
-    # Else if the counter is done
-        if(counterHours == 0 and counterMinutes == 0 and counterSeconds == 0):
-            # Get data sequence from change_state() function and set the varibles
-            notification, sound, passedHours, passedMinutes, passedSeconds = change_state()
-            # Using the variables which were set according to the state, print the message,
-            # play the sound, and set the counter
-            print(notification)
+def play_sound(sound):
+    try:
+        winsound.PlaySound(sound, winsound.SND_ASYNC)
+    except Exception:
+        # If error, try method for playing sound on linux
+        try:
+            os.system("aplay " + sound + ".wav&")
+        except Exception:
+            # If error, try method for playing sound on a Mac
             try:
-                winsound.PlaySound(sound, winsound.SND_ASYNC)
-            except Exception:
-                # If error, try method for playing sound on linux
-                try:
-                    os.system("aplay " + sound + ".wav&")
-                except Exception:
-                    # If error, try method for playing sound on a Mac
-                    try:
-                        os.system("afplay " + sound + ".wav&")
-                    except Exception as e:
-                        print(e)
-            counterHours, counterMinutes, counterSeconds = passedHours, passedMinutes, passedSeconds
-            isWorking = not isWorking
-            time.sleep(1)
+                os.system("afplay " + sound + ".wav&")
+            except Exception as e:
+                print(e)
+            
+def zero_pad_int(integer):
+    if integer < 10:
+        return "0"+str(integer)
+    else:
+        return str(integer)
+
+def cycle_time(string):
+    hours = re.search('(\d+)h',string)
+    if hours:
+        hours = hours.group().replace('h','')
+    minutes = re.search('h/d+$|\d+m|\d+$', string) # Match 1+ digits after h followed by end of line, 1+ digits followed by m, or 1+ digits followed by end of line
+    if minutes:
+        minutes = minutes.group().replace('m','')
+    # Return time 
+    if hours and minutes:
+        return int(hours)*60 + int(minutes)
+    elif hours and not minutes:
+        return int(hours)*60
+    else:
+        return int(minutes)
+ 
+# -- PROGRAM START -------
+print('Press Ctrl+C to exit during the countdown.')
+string = ("Enter times for work, break, and long break in minutes, and the number of iterations before the long break time activates,"
+    " separated by spaces.\n\ne.g. \"1h45 15 30 3\" to work for 1 hour 45 minutes with a 15 min break for 3 cycles, followed by a cycle with a 30 minute break:")
+print(string)
+work_time, break_time, long_time, iterations = set_times()
+
+while True:
+    for loop in range(int(iterations)+1):
+        start_timer(work_time, 'work')
+        if loop < int(iterations):
+            start_timer(break_time, 'break')
+        else:
+            start_timer(long_time, 'long break')
